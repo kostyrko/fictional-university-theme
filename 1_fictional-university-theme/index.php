85a9033318d93762b72d9ec558604187ea04c964
kostyrko <mkostyrko@gmail.com>
2020-12-03 19:02:34 +0100

    first php & wordpress functions

--- 1_fictional-university-theme/index.php
+++ 1_fictional-university-theme/index.php
@@ -1,1 +1,13 @@
-This our amazing custom theme
+<?php
+  function greet($name, $planet)
+  {
+      echo "<p>Hi, my name $name and I from planet $planet</p>";
+  }
+
+  greet('R2D2', 'Naboo');
+  greet('C-3PO', 'Tatooine');
+?>
+
+<h1> <?php bloginfo('name'); ?> </h1>
+
+<p> <?php bloginfo('description'); ?> </p>
