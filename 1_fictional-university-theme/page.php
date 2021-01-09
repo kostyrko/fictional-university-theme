af9e1d16d3785f9cba1c1aac3991486a3eaa8fe1
kostyrko <mkostyrko@gmail.com>
2021-01-09 18:22:45 +0100

    Add dynamic pages_subpages inside menu/links

--- 1_fictional-university-theme/page.php
+++ 1_fictional-university-theme/page.php
@@ -1,10 +1,8 @@
 <?php
 
   get_header();
-  
   while (have_posts()) {
       the_post(); ?>
-    
     <div class="page-banner">
         <!-- get_theme_file_uri('') -->
         <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>);"></div>
@@ -15,28 +13,45 @@
           <div class="page-banner__intro">
             <p>REPLACE LATER</p>
           </div>
-        </div>  
+        </div>
       </div>
 
       <div class="container container--narrow page-section">
-        <?php 
+        <?php
         $theParent = wp_get_post_parent_id(get_the_ID());
-          if($theParent) { ?>
+      // $theParent zwraca ID rodzica lub 0 jeśli brak takiego (tzn rodzica)
+      if ($theParent) { ?>
             <div class="metabox metabox--position-up metabox--with-home-link">
               <p><a class="metabox__blog-home-link" href="<?php echo get_the_permalink($theParent) ?>"><i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title($theParent) ?></a> <span class="metabox__main"><?php the_title(); ?></span></p>
             </div>
-          <?php }
-        ?>
-
-        
-        
-        <!-- <div class="page-links">
-          <h2 class="page-links__title"><a href="#">About Us</a></h2>
+          <?php } ?>
+    <?php
+    // pobierz id stron ale tylko będące dziećmi
+      $testArray = get_pages(array(
+        'child_of'=> get_the_ID()
+      ));
+      // jeśli posiada rodzica albo posiada dzieci to wyświetl
+      if ($theParent or $testArray) { ?>
+        <div class="page-links">
+          <h2 class="page-links__title"><a href="<?php get_the_permalink($theParent) ?>"> <?php echo get_the_title($theParent); ?> </a></h2>
           <ul class="min-list">
-            <li class="current_page_item"><a href="#">Our History</a></li>
-            <li><a href="#">Our Goals</a></li>
+            <?php
+            if ($theParent) {
+                $findChildrenOf = $theParent;
+            } else {
+                $findChildrenOf = get_the_ID();
+            }
+            // tworzy listę wszystkich stron oraz linków do nich
+            // wp_list_pages();
+            // przyjmuje argumenty w postaci tablicy asocjacyjnej
+            wp_list_pages(array(
+              'title_li' => null,
+              'child_of' => $findChildrenOf
+            ));
+            ?>
           </ul>
-        </div> -->
+        </div>
+          <?php } ?>
 
         <div class="generic-content">
           <?php the_content() ?>
