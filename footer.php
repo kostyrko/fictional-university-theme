ac4464f13af14c9a56e5c7d702f0db29e0e8738b
kostyrko <mkostyrko@gmail.com>
2021-03-07 18:52:04 +0100

    Update footer

--- footer.php
+++ footer.php
@@ -2,22 +2,18 @@
       <div class="site-footer__inner container container--narrow">
         <div class="group">
           <div class="site-footer__col-one">
-            <h1 class="school-logo-text school-logo-text--alt-color">
-              <a href="<?php echo site_url() ?>"><strong>Fictional</strong> University</a>
-            </h1>
-            <p><a class="site-footer__link" href="#">555.555.5555</a></p>
+            <h2 class="school-logo-text school-logo-text--alt-color">
+              <a href="<?php echo site_url() ?>">Galeria <strong>JAK</strong></a>
+            </h2>
+            <p >przestrzeń wystawiennicza Fundacji Jak Malowana</p>
+            <p><a class="site-footer__link" href="tel:693 083 144">693 083 144</a></p>
+            <p><a class="site-footer__link" href="https://goo.gl/maps/guNVnkAgsGmTvX3S6" target="_blank">ul. Święty Marcin 37, Poznań 61-806</a></p>
           </div>
 
           <div class="site-footer__col-two-three-group">
             <div class="site-footer__col-two">
-              <h3 class="headline headline--small">Explore</h3>
+              <h3 class="headline headline--small">Zapraszamy</h3>
               <nav class="nav-list">
-                <!-- <ul>
-                  <li><a href="<?php echo site_url('/about-us/') ?>">About Us</a></li>
-                  <li><a href="#">Programs</a></li>
-                  <li><a href="#">Events</a></li>
-                  <li><a href="#">Campuses</a></li>
-                </ul> -->
                 <?php 
                   wp_nav_menu(array(
                     'theme_location' => 'footerLocationOne'
@@ -26,41 +22,19 @@
               </nav>
             </div>
 
-            <div class="site-footer__col-three">
-              <h3 class="headline headline--small">Learn</h3>
-              <nav class="nav-list">
-                <!-- <ul>
-                  <li><a href="#">Legal</a></li>
-                  <li><a href="<?php echo site_url('/privacy-policy/') ?>">Privacy</a></li>
-                  <li><a href="#">Careers</a></li>
-                </ul> -->
-                <?php 
-                  wp_nav_menu(array(
-                    'theme_location' => 'footerLocationTwo'
-                  ))
-                ?>
-              </nav>
-            </div>
-          </div>
 
           <div class="site-footer__col-four">
-            <h3 class="headline headline--small">Connect With Us</h3>
+            <h3 class="headline headline--small">Odwiedź nas na:</h3>
             <nav>
               <ul class="min-list social-icons-list group">
                 <li>
-                  <a href="#" class="social-color-facebook"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                  <a href="https://www.facebook.com/galeriajak" class="social-color-facebook"><i class="fa fa-facebook" aria-hidden="true"></i></a>
                 </li>
                 <li>
-                  <a href="#" class="social-color-twitter"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+                  <a href="https://www.youtube.com/channel/UCK8QHZYjp2P9A_H4yYk276g   " class="social-color-youtube"><i class="fa fa-youtube" aria-hidden="true"></i></a>
                 </li>
                 <li>
-                  <a href="#" class="social-color-youtube"><i class="fa fa-youtube" aria-hidden="true"></i></a>
-                </li>
-                <li>
-                  <a href="#" class="social-color-linkedin"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
-                </li>
-                <li>
-                  <a href="#" class="social-color-instagram"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                  <a href="https://www.instagram.com/galeria_jak/" class="social-color-instagram"><i class="fa fa-instagram" aria-hidden="true"></i></a>
                 </li>
               </ul>
             </nav>
