069c04b126085091ffbde652fe1c101e577d9ba4
kostyrko <mkostyrko@gmail.com>
2021-03-14 15:18:19 +0100

    change dest folder + rename

--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,19 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const sourcemaps = require('gulp-sourcemaps');
+const rename = require('gulp-rename');
+
 
 gulp.task('sass', function(){
-  return gulp.src('./sass/main.scss') 
+  return gulp.src('./sass/main.scss')
     .pipe(sourcemaps.init())
     .pipe(sass({
         outputStyle: 'expanded',
-        sourceComments: 'map' 
+        sourceComments: 'map'
     }).on("error", sass.logError))
-    .pipe(sourcemaps.write()) 
-    .pipe(gulp.dest('css'))  
+    .pipe(rename('style.css'))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./'))
 });
 
 gulp.task('watch', function() {
